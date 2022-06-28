9a6bbe761943b4fe33a87f61892496fb076d4523
Tomas Pilat <tomas.pilat@disig.sk>
2022-06-28 08:59:50 +0200

    DbOperation moved to Envelope.Model

---
+++ src/Envelope.Model/DbOperation.cs
@@ -0,0 +1,10 @@
+﻿namespace Envelope.Model;
+
+public enum DbOperation
+{
+	None = 0,
+	Create = 1,
+	Update = 2,
+	Delete = 3,
+	Read = 4
+}
---
+++ src/Envelope.Model/IDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Envelope.Model;
+
+public interface IDto
+{
+	int DbOperation { get; }
+
+	Dictionary<string, string> Acl { get; }
+	
+	Guid? ConcurrencyToken { get; }
+}
